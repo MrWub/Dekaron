718669a8a3ccf68d105501aff08bc911e155ebc2
wyt <914554688@qq.com>
2016-07-29 21:51:30 +0800

    mobs空壳 7.27 21:51

--- src/com/github/MrWub/Dekaron/mobs/Mob.java
+++ src/com/github/MrWub/Dekaron/mobs/Mob.java
@@ -1,5 +1,50 @@
 package com.github.MrWub.Dekaron.mobs;
 
-public class Mob {
+import java.io.EOFException;
 
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+
+public class Mob {
+	private LivingEntity handle = null;
+	private MobType mobType = null;
+	private int flag = 0;
+	private String name = "DekaronMob";
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static Mob spawnMob(MobType mt, Location loc){
+		Mob mob = new Mob();
+		mob.handle = (LivingEntity) loc.getWorld().spawnEntity(loc, mt.getEntityType());
+		return mob;
+	}
+	
+	public LivingEntity getHandle() {
+		return handle;
+	}
+
+	public MobType getMobType() {
+		return mobType;
+	}
+
+	public void setMobType(MobType mobType) {
+		this.mobType = mobType;
+	}
+
+	public int getFlag() {
+		return flag;
+	}
+
+	public void setFlag(int flag) {
+		this.flag = flag;
+	}
+	
+	
 }
--- src/com/github/MrWub/Dekaron/mobs/MobsManager.java
+++ src/com/github/MrWub/Dekaron/mobs/MobsManager.java
@@ -1,5 +1,15 @@
 package com.github.MrWub.Dekaron.mobs;
 
-public class MobsManager {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.bukkit.Location;
+
+public class MobsManager {
+	private static List<Mob> mobs = new ArrayList<Mob>();
+	public static Mob spawnMob(Location loc, MobType mt) {
+		Mob mob = Mob.spawnMob(mt, loc);
+		mobs.add(mob);
+		return mob;
+	}
 }
