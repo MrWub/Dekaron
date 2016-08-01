d58a8777ac89cf652983e1911d4c9121e025603a
sckbee <sckbee@DESKTOP-4VK5USH>
2016-08-01 10:40:29 +0800

    添加Bukkit版本枚举

---
+++ src/com/github/MrWub/Dekaron/BukkitVersionType.java
@@ -0,0 +1,9 @@
+package com.github.MrWub.Dekaron;
+
+public enum BukkitVersionType {
+	V1_10_R1("V1_10_R1");
+	String version;
+	private BukkitVersionType(String version){
+		this.version=version;
+	}
+}
