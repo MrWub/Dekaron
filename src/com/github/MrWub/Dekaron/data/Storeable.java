275a43bcc78c560a76a483301e8dd474dbae16ff
sckbee <sckbee@DESKTOP-4VK5USH>
2016-08-01 13:31:00 +0800

    添加方法,用于测试git

--- src/com/github/MrWub/Dekaron/data/Storeable.java
+++ src/com/github/MrWub/Dekaron/data/Storeable.java
@@ -1,5 +1,14 @@
 package com.github.MrWub.Dekaron.data;
 
+/**
+ * 
+ * @author sckbee
+ *	@version 1.0.0.0
+ */
 public interface Storeable {
-
+	/**
+	 * 
+	 * @return 返回字节数组形式方便储存
+	 */
+	public byte[] toBytes();
 }
